65fe19f1a53c49001a4e6c3b39e2c8d678981482
Jake Gage <jake.gage@gmail.com>
2013-10-16 16:44:00 -0500

    adds jQuery loading
    I'm re-adding jQuery library loading, to facilitate copy-to-clipboard
    functionality.

--- src/javascript/gmail-link-bookmarklet.js
+++ src/javascript/gmail-link-bookmarklet.js
@@ -1,6 +1,27 @@
-(function() {
-	var hash = window.location["hash"];
-	var threadID = hash.split("/")[1];
-	threadURL = "https://mail.google.com/a/umn.edu/#inbox/" + threadID;
-	window.prompt ("Copy to clipboard: Ctrl+C, Enter", threadURL);
+(function(){
+	
+	if ( window.jQuery === undefined ) {
+		var done = false;
+		var script = document.createElement("script");
+		script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js";
+		script.onload = script.onreadystatechange = function(){
+			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+				done = true;
+				initMyBookmarklet();
+			}
+		};
+		document.getElementsByTagName("head")[0].appendChild(script);
+	} else {
+		initMyBookmarklet();
+	}
+	
+	function initMyBookmarklet() {
+		(window.myBookmarklet = function() {
+			var hash = window.location["hash"];
+			var threadID = hash.split("/")[1];
+			threadURL = "https://mail.google.com/a/umn.edu/#inbox/" + threadID;
+			window.prompt ("Copy to clipboard: Ctrl+C, Enter", threadURL);
+		})();
+	}
+	
 })();
