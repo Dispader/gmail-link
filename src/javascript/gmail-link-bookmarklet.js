f8065a83bc375f0fcb60b8741742937776842280
Jake Gage <jake.gage@gmail.com>
2013-10-16 15:56:02 -0500

    removes debugging thread ID alert

--- src/javascript/gmail-link-bookmarklet.js
+++ src/javascript/gmail-link-bookmarklet.js
@@ -1,7 +1,6 @@
 (window.myBookmarklet = function() {
 	var hash = window.location["hash"];
 	var threadID = hash.split("/")[1];
-	alert(threadID);
 	threadURL = "https://mail.google.com/a/umn.edu/#inbox/" + threadID;
 	alert(threadURL);
 })();
