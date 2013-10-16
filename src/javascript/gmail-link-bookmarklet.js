03971a9c101db4522222f7bc60bead22b975327b
Jake Gage <jake.gage@gmail.com>
2013-10-16 15:48:30 -0500

    correctly extracts Google Mail thread ID

--- src/javascript/gmail-link-bookmarklet.js
+++ src/javascript/gmail-link-bookmarklet.js
@@ -17,8 +17,10 @@
 	
 	function initMyBookmarklet() {
 		(window.myBookmarklet = function() {
-  			var hash = window.location["hash"]
-  			alert(hash);
+			var hash = window.location["hash"];
+			alert(hash);
+  			var threadID = hash.split("/")[1];
+  			alert(threadID);
 		})();
 	}
 
