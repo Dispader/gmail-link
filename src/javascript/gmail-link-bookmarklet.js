d7149e6f567f8a313c5fb94e18b3ebe8e12c9b13
Jake Gage <jake.gage@gmail.com>
2013-10-16 16:53:29 -0500

    adds JavaScript URL shortener library loading
    Adds loading the jQuery-URL-shortener library.  For more information,
    see https://github.com/hayageek/jQuery-URL-shortener

--- src/javascript/gmail-link-bookmarklet.js
+++ src/javascript/gmail-link-bookmarklet.js
@@ -2,15 +2,24 @@
 	
 	if ( window.jQuery === undefined ) {
 		var done = false;
-		var script = document.createElement("script");
-		script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js";
-		script.onload = script.onreadystatechange = function(){
+		var jQueryScript = document.createElement("script");
+		jQueryScript.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js";
+		jQueryScript.onload = jQueryScript.onreadystatechange = function(){
 			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
 				done = true;
 				initMyBookmarklet();
 			}
 		};
-		document.getElementsByTagName("head")[0].appendChild(script);
+		var urlShortenerScript = document.getElementsByTagName("head")[0].appendChild(urlShortenerScript);
+		var urlShortenerScript = document.createElement("script");
+		urlShortenerScript.src = "https://raw.github.com/hayageek/jQuery-URL-shortener/master/jquery.urlshortener.js";
+		urlShortenerScript.onload = urlShortenerScript.onreadystatechange = function(){
+			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+				done = true;
+				initMyBookmarklet();
+			}
+		};
+		document.getElementsByTagName("head")[0].appendChild(urlShortenerScript);		
 	} else {
 		initMyBookmarklet();
 	}
