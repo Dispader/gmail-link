61d6d061211bbaa4deaebb7bbfb7733bf6e16095
Jake Gage <jake.gage@gmail.com>
2013-10-16 15:08:10 -0500

    fetches jQuery from Google secure CDN

--- src/javascript/gmail-link-bookmarklet.js
+++ src/javascript/gmail-link-bookmarklet.js
@@ -3,7 +3,7 @@
 	if ( window.jQuery === undefined ) {
 		var done = false;
 		var script = document.createElement("script");
-		script.src = "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js";
+		script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js";
 		script.onload = script.onreadystatechange = function(){
 			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
 				done = true;
@@ -17,7 +17,8 @@
 	
 	function initMyBookmarklet() {
 		(window.myBookmarklet = function() {
-			alert("your JavaScript code goes here!");
+			var locationURL = window.location.pathname;
+			alert(locationURL);
 		})();
 	}
 
