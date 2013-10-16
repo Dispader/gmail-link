b9f3e697356e2362a20df9f445aa1d25e8dfb4c7
Jake Gage <jake.gage@gmail.com>
2013-10-16 15:50:26 -0500

    removes jQuery loading
    It doesn't appear that we have a need for jQuery to extract or manipulate the URL string.  This commit removes the CDN loading code to simplify the bookmarklet function.

--- src/javascript/gmail-link-bookmarklet.js
+++ src/javascript/gmail-link-bookmarklet.js
@@ -1,27 +1,6 @@
-(function(){
-
-	if ( window.jQuery === undefined ) {
-		var done = false;
-		var script = document.createElement("script");
-		script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js";
-		script.onload = script.onreadystatechange = function(){
-			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
-				done = true;
-				initMyBookmarklet();
-			}
-		};
-		document.getElementsByTagName("head")[0].appendChild(script);
-	} else {
-		initMyBookmarklet();
-	}
-	
-	function initMyBookmarklet() {
-		(window.myBookmarklet = function() {
-			var hash = window.location["hash"];
-			alert(hash);
-  			var threadID = hash.split("/")[1];
-  			alert(threadID);
-		})();
-	}
-
+(window.myBookmarklet = function() {
+	var hash = window.location["hash"];
+	alert(hash);
+	var threadID = hash.split("/")[1];
+	alert(threadID);
 })();
