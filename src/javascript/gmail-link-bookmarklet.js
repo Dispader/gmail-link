b8877326e84998951cc7b1892bc5c29489dec8f4
Jake Gage <jake.gage@gmail.com>
2013-10-16 15:55:24 -0500

    creates URL link to thread

--- src/javascript/gmail-link-bookmarklet.js
+++ src/javascript/gmail-link-bookmarklet.js
@@ -2,4 +2,6 @@
 	var hash = window.location["hash"];
 	var threadID = hash.split("/")[1];
 	alert(threadID);
+	threadURL = "https://mail.google.com/a/umn.edu/#inbox/" + threadID;
+	alert(threadURL);
 })();
